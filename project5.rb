beb1b66ba911c17ff7de65f3b0b0352b47558903
Zach Fudge <zacheryfudge@gmail.com>
2018-01-14 01:09:45 -0700

    problem 5 description

--- project5.rb
+++ project5.rb
@@ -1,7 +1,13 @@
+# The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)^2 = 55^2 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 simpleSum = 0
 squareSum = 0
 for n in 1..100
 	simpleSum += n
 	squareSum += n**2
 end
-puts simpleSum ** 2 - squareSum
+puts "The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is #{simpleSum ** 2 - squareSum}."
