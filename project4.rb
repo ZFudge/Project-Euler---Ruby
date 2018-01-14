7a7b1058c2c00ae6b23fe777c0741469ff7107a3
Zach Fudge <zacheryfudge@gmail.com>
2018-01-14 02:22:49 -0700

    Added description. Refactored loop.

--- project4.rb
+++ project4.rb
@@ -1,12 +1,16 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+done = false
+num = 100
+
 def detDiv(num)
 	return num % 20 == 0 && num % 19 == 0 && num % 18 == 0 && num % 17 == 0 && num % 16 == 0 && num % 15 == 0 && num % 14 == 0 && num % 13 == 0 && num % 12 == 0 && num % 11 == 0
 end
-unanswered = true
-num = 100
-while unanswered
-	if detDiv(num)
-		unanswered = false;
-		puts num
-	end
+
+while not done
 	num += 1
+	if detDiv(num) then done = true end
 end
+
+puts "The smallest positive number that can be evenly divided by all of the numbers from 1 to 20 is #{num}"
