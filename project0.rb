a3d1b475310fa8dff077d874ad626a91c11b4142
Zach Fudge <zacheryfudge@gmail.com>
2018-01-14 02:30:50 -0700

    added problem description

--- project0.rb
+++ project0.rb
@@ -1,5 +1,10 @@
-$sum = 0
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+sum = 0
+
 for n in (0..999)
-		$sum += n unless n % 3 != 0 && n % 5 != 0
+		sum += n unless n % 3 != 0 && n % 5 != 0
 end
-puts $sum
+
+puts "The sum of all multiples of 3 and 5 below 1000 is #{sum}"
