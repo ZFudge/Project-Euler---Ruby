ebf70f0cc7eb84f71b964d2f7035f18fc849a27a
Zach Fudge <zacheryfudge@gmail.com>
2018-01-15 01:19:57 -0700

    problem 19

---
+++ project19.rb
@@ -0,0 +1,16 @@
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+# Find the sum of the digits in the number 100!
+
+def factorial(num)
+	if num > 1 then num *= (factorial(num-1)) end
+	return num
+end
+
+digits = (factorial(100)).to_s
+sum = 0
+digits.split("").each { |n| sum += n.to_i }
+
+
+puts " The sum of the digit in the number 100! is #{sum}"
